d1ce6da3a23b8ec3707d923ca92c970b0223da47
Rituparna <rituparnabanik0233@gmail.com>
2025-11-08 21:08:31 +0530

    Add ESLint configuration and fix lint setup

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+    },
+  },
+];
